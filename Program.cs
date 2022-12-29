9d70725fc5fe4975adbc7f7e2187b9d1c7504512
Daniil <daniil.gudkows@gmail.com>
2022-12-29 21:47:37 +0300

    add metods

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//количество нужных строк в массиве
+int NumRightStrings(string[] array, int leght)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if ( array[i].Length<leght ) result++;
+    }
+    return result;
+}
+//запись нового массива из подходящих строк
+string[] RecordRightStrings (string[] array , int count, int leght)
+{
+    int index = 0;
+    string[] result = new string[count];
+    for (int i = 0; i < count; i++)
+    {
+        if (array[i].Length< leght) 
+        {
+            result[index] = array[i];
+            index++;
+        }
+    }
+    return result;
+}
