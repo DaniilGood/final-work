a509af1dc0196efb89047ba9f4ba2ac09c24b570
Daniil <daniil.gudkows@gmail.com>
2022-12-30 22:57:53 +0300

    working version

--- Program.cs
+++ Program.cs
@@ -4,18 +4,18 @@ int NumRightStrings(string[] array, int leght)
     int result = 0;
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        if ( array[i].Length<leght ) result++;
+        if (array[i].Length <= leght) result++;
     }
     return result;
 }
 //запись нового массива из подходящих строк
-string[] RecordRightStrings (string[] array , int count, int leght)
+string[] RecordRightStrings(string[] array, int count, int leght)
 {
     int index = 0;
     string[] result = new string[count];
-    for (int i = 0; i < count; i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        if (array[i].Length< leght) 
+        if (array[i].Length <= leght)
         {
             result[index] = array[i];
             index++;
@@ -23,3 +23,21 @@ string[] RecordRightStrings (string[] array , int count, int leght)
     }
     return result;
 }
+//печать массива
+void PrintArray(string[] col)
+{
+    Console.Write("[");
+    int Length = col.Length;
+    int position = 0;
+    while(position<Length)
+    {
+        Console.Write(col[position]+ ", ");
+        position++;
+    }
+    Console.Write("]");
+}
+
+string[] array = {"ffg", "lol", "gg wp","hamuda habibi", "UwU","tgt", "gg"};
+int leght = 3;
+int count = NumRightStrings(array, leght);
+PrintArray(RecordRightStrings(array, count, leght));
